a38e5913b44dadf76fd019fad444f69d1c890edc
Taylor Huston <taylor.a.huston@gmail.com>
2023-02-14 18:48:02 -0500

    Variables

---
+++ Chap2/variables.py
@@ -0,0 +1,20 @@
+x, y, z = 1, 2, 3
+
+print(x, y, z)
+
+universe_age = 14_000_000_000
+print(universe_age)
+
+#this is a comment
+
+
+x=10
+y=3
+
+print(x+y)  
+print(x-y)  
+print(x*y)  
+print(x%y)  
+print(x**y)  
+print(x/y)  
+print(x//y)  
