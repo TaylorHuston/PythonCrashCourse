1e9546d6a2e374e936a7e0513b305a8b3355cb79
Taylor Huston <taylor.a.huston@gmail.com>
2023-03-08 11:20:48 -0500

    Add comments of expected output

--- Chap3/lists.py
+++ Chap3/lists.py
@@ -8,40 +8,41 @@ message = f"My first bicycle was a {bicycles[0].title()}."
 print(message)
 
 motorcycles = ['honda', 'yamaha', 'suzuki']
-print(motorcycles)
+print(motorcycles) #['honda', 'yamaha', 'suzuki']
 
 motorcycles.append('ducati')
-print(motorcycles)
+print(motorcycles) #['honda', 'yamaha', 'suzuki', 'ducati']
 
 motorcycles = ['honda', 'yamaha', 'suzuki']
 
 motorcycles.insert(0, 'ducati')
-print(motorcycles)
+print(motorcycles) #['ducati', 'honda', 'yamaha', 'suzuki']
 
 del motorcycles[0]
-print(motorcycles)
+print(motorcycles) #['honda', 'yamaha', 'suzuki']
 
 popped_motorcycle = motorcycles.pop()
-print(motorcycles)
-print(popped_motorcycle)
+print(motorcycles) #['honda', 'yamaha']
+print(popped_motorcycle) #suzuki
 
 motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
-print(motorcycles)
+print(motorcycles) #['honda', 'yamaha', 'suzuki', 'ducati']
 
 motorcycles.remove('ducati')
-print(motorcycles)
+print(motorcycles) #['honda', 'yamaha', 'suzuki']
 
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 cars.sort()
-print(cars)
+print(cars) #['audi', 'bmw', 'subaru', 'toyota']
 
 cars.sort(reverse=True)
-print(cars)
-print(cars)
+print(cars) #['toyota', 'subaru', 'bmw', 'audi']
 
 cars = ['bmw', 'audi', 'toyota', 'subaru']
-print(cars)
+print(cars) #['bmw', 'audi', 'toyota', 'subaru']
 
 cars.reverse()
-print(cars)
-print(len(cars))
+print(cars) #['subaru', 'toyota', 'audi', 'bmw']
+print(len(cars)) #4
+
+print(cars[-1]) #bmw
