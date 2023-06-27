654b136628a21e13ef046cc9f6f3861603afafd9
Taylor Huston <taylor.a.huston@gmail.com>
2023-06-26 22:51:47 -0800

    Add Python devcontainer

--- Chap6/dictionairies.py
+++ Chap6/dictionairies.py
@@ -12,3 +12,7 @@ print(alien_0) #{'color': 'green', 'points': 5, 'x_position': 0, 'y_position': 2
 
 alien_0 = {}
 print(alien_0) #{}
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0) #{'x_position': 0, 'y_position': 25}
